3880c83134c00dac2dc3c06683d3e272503bae48
Kennynyamai <kennynyamai73@gmail.com>
2023-09-13 12:43:34 +0300

    deployment6

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////rest_api/mydatabase.db'
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
